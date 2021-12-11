7a6485120ffed9c3985b3dbe49320b227e609ba9
nataliaalvs <natalia.reges6@gmail.com>
2021-12-11 19:58:16 -0300

    mudando rotas

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,6 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\AuthController;
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
@@ -31,10 +20,5 @@ Route::get('/perfil', function () {
 })->name('perfil')->middleware(['auth']);
 
 Route::resource('news', NewsController::class);
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
