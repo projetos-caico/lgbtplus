8528ac10af223fe02ea32830c6edcc71598c3c92
nataliaalvs <natalia.reges6@gmail.com>
2022-01-31 08:57:11 -0300

    add telefone p contato

--- resources/views/site/contact.blade.php
+++ resources/views/site/contact.blade.php
@@ -65,7 +65,7 @@
             <div class="col">
                 <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-chat-dots"></i></div>
                 <div class="h5 mb-2">Fale com a gente</div>
-                <p class="text-muted mb-0">Chat live with one of our support specialists.</p>
+                <p class="text-muted mb-0">Converse com alguém da nossa equipe.</p>
             </div>
             {{-- <div class="col">
                 <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-people"></i></div>
@@ -75,12 +75,12 @@
             <div class="col">
                 <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-question-circle"></i></div>
                 <div class="h5">Suporte</div>
-                <p class="text-muted mb-0">Browse FAQ's and support articles to find solutions.</p>
+                <p class="text-muted mb-0">Tire dúvidas, compartilhe informações.</p>
             </div>
             <div class="col">
                 <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-telephone"></i></div>
-                <div class="h5">Ligue</div>
-                <p class="text-muted mb-0">Call us during normal business hours at (555) 892-9403.</p>
+                <div class="h5">Ligue/ WhatsApp</div>
+                <p class="text-muted mb-0">(81) 99766-5814.</p>
             </div>
         </div>
     </div>
