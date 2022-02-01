a1bf6b10224b63bad24e00f60aab3d068c34fc77
nataliaalvs <natalia.reges6@gmail.com>
2022-02-01 14:40:51 -0300

    att logo parceiros

--- resources/views/site/partners.blade.php
+++ resources/views/site/partners.blade.php
@@ -1,36 +1,32 @@
-<section class="rounded mx-auto d-block">
-    <div class="container px-5 my-5">
-        <div class="mb-2 fw-bold text-lgbt-purple text-center" style="text-shadow: 0 0 5px rgba(238, 255, 3, 0.432)">Realização</div>
-            <div class="images">
-                {{-- text-shadow: 0 0 5px rgba(167, 36, 219, 0.575) --}}
-                <div class="border-0 h-100">
-                        
-                        <img class="img-thumbnail" src="{{asset('images/logoIFRN.png')}}" alt="Logo IFRN" style="height: 120px">
-                </div>
-            </div>
+<section class="py-5 mt-auto">
+    <div class="row">
+        <div class="col-md-4 px-5 py-2">
+            <p class=" fw-bold text-lgbt-purple">Parceiros</p>
+            <img src="{{asset('images/logo-cetra.png')}}" alt="">
+            <img src="{{asset('images/logo-centro-sabia.png')}}" alt="">
+        </div>
+        <div class="col-md-4 ml-auto px-5 py-2">
+            <p class="fw-bold text-lgbt-purple">Realização</p>
+            <img class="img-thumbnail" src="{{asset('images/logoIFRN.png')}}" alt="Logo IFRN">
         </div>
     </div>
 </section>
 
-<section class="py-5 mt-auto ">
-    <div class="container px-5 my-2">
-        <div class="mb-2 fw-bold text-lgbt-purple">Parceiros</div>
-            <div class="images">
+{{-- <section class="py-5 mt-auto">
+    <div class="container px-5 my-2 row">
+        <div class="mb-2 fw-bold text-lgbt-purple col-sm-6 col-md-5 col-lg-6">Parceiros</div>
+            <div class="images col-sm-6 col-md-5 col-lg-6">
                 <img class="px-2 img-thumbnail" src="{{asset('images/logo-centro-sabia.png')}}" alt="Logo Centro Sabiá">
                 <img class="img-thumbnail" src="{{asset('images/logo-cetra.png')}}" alt="Logo Cetra">
+
             </div>
-            {{-- <div class="d-flex align-items-center justify-content-between flex-column flex-xl-row text-center text-xl-start">
-                <div class="mb-4 mb-xl-0">
-                    <div class="text-white-50"></div>
-                </div>  --}}
-                {{-- <div class="ms-xl-4">
-                    <div class="input-group mb-2">
-                        <input class="form-control" type="text" placeholder="Email address..." aria-label="Email address..." aria-describedby="button-newsletter" />
-                        <button class="btn btn-outline-light" id="button-newsletter" type="button">Sign up</button>
-                    </div>
-                    <div class="small text-white-50">We care about privacy, and will never share your data.</div>
-                </div> --}}
-            </div>
-    </div>
-</section>
+        
+            <div class="mb-2 fw-bold text-lgbt-purple col-sm-6 col-md-5 offset-md-2col-lg-6 offset-lg-0">Realização</div>
+                <div class="images col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0">
+                    <img class="img-thumbnail" src="{{asset('images/logoIFRN.png')}}" alt="Logo IFRN">
+                </div>
+        </div>
+</section> --}}
+
+  
 
