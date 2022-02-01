70700e210f6f4be0e68aa35a0104790163ab66ae
nataliaalvs <natalia.reges6@gmail.com>
2022-02-01 16:21:30 -0300

    parc. e realização

--- resources/views/site/partners.blade.php
+++ resources/views/site/partners.blade.php
@@ -1,38 +1,3 @@
-{{-- <section class="py-5 mt-auto">
-    <div class="row">
-        <div class="col-md-4 px-5 py-2">
-            <p class=" fw-bold text-lgbt-purple">Parceiros</p>
-            <img src="{{ asset('images/logo-cetra.png') }}" alt="">
-            <img src="{{ asset('images/logo-centro-sabia.png') }}" alt="">
-        </div>
-        <div class="col-md-4 ml-auto px-5 py-2">
-            <p class="fw-bold text-lgbt-purple">Realização</p>
-            <img class="img-thumbnail" src="{{ asset('images/logoIFRN.png') }}" alt="Logo IFRN">
-        </div>
-    </div>
-</section>
-
-<section class="py-5 row mt-5">
-    <div class="col-sm-6 col-md-6 mb-5 ">
-        <div class="  h-100">
-            <div class="p-4">
-                {{-- <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title mb-2">Bissexual</h5></a>
-                                <p class="card-text mb-0">Pessoa que sente atração sexual e/ou afeto por pessoas do gênero feminino e do gênero masculino</p> 
-                <p class="px-3 fw-bold text-lgbt-purple">Parceiros</p>
-                <img class="px-2" src="{{ asset('images/logo-cetra.png') }}" alt="">
-                <img class="px-2" src="{{ asset('images/logo-centro-sabia.png') }}" alt="">
-            </div>
-        </div>
-    </div>
-</section> --}}
-
-
-
-
-
-
-
-
 <section class="py-5">
     <div class="container my-5">
         <div class="row mt-2">
