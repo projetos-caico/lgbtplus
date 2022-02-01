e3b20fb4d2e44ea3925595683950c4832b4fcafa
nataliaalvs <natalia.reges6@gmail.com>
2022-02-01 16:20:39 -0300

    tabela emails

--- resources/views/site/partners.blade.php
+++ resources/views/site/partners.blade.php
@@ -1,32 +1,82 @@
-<section class="py-5 mt-auto">
+{{-- <section class="py-5 mt-auto">
     <div class="row">
         <div class="col-md-4 px-5 py-2">
             <p class=" fw-bold text-lgbt-purple">Parceiros</p>
-            <img src="{{asset('images/logo-cetra.png')}}" alt="">
-            <img src="{{asset('images/logo-centro-sabia.png')}}" alt="">
+            <img src="{{ asset('images/logo-cetra.png') }}" alt="">
+            <img src="{{ asset('images/logo-centro-sabia.png') }}" alt="">
         </div>
         <div class="col-md-4 ml-auto px-5 py-2">
             <p class="fw-bold text-lgbt-purple">Realização</p>
-            <img class="img-thumbnail" src="{{asset('images/logoIFRN.png')}}" alt="Logo IFRN">
+            <img class="img-thumbnail" src="{{ asset('images/logoIFRN.png') }}" alt="Logo IFRN">
         </div>
     </div>
 </section>
 
-{{-- <section class="py-5 mt-auto">
-    <div class="container px-5 my-2 row">
-        <div class="mb-2 fw-bold text-lgbt-purple col-sm-6 col-md-5 col-lg-6">Parceiros</div>
-            <div class="images col-sm-6 col-md-5 col-lg-6">
-                <img class="px-2 img-thumbnail" src="{{asset('images/logo-centro-sabia.png')}}" alt="Logo Centro Sabiá">
-                <img class="img-thumbnail" src="{{asset('images/logo-cetra.png')}}" alt="Logo Cetra">
-
+<section class="py-5 row mt-5">
+    <div class="col-sm-6 col-md-6 mb-5 ">
+        <div class="  h-100">
+            <div class="p-4">
+                {{-- <a class="text-decoration-none link-dark stretched-link" href="#!"><h5 class="card-title mb-2">Bissexual</h5></a>
+                                <p class="card-text mb-0">Pessoa que sente atração sexual e/ou afeto por pessoas do gênero feminino e do gênero masculino</p> 
+                <p class="px-3 fw-bold text-lgbt-purple">Parceiros</p>
+                <img class="px-2" src="{{ asset('images/logo-cetra.png') }}" alt="">
+                <img class="px-2" src="{{ asset('images/logo-centro-sabia.png') }}" alt="">
             </div>
-        
-            <div class="mb-2 fw-bold text-lgbt-purple col-sm-6 col-md-5 offset-md-2col-lg-6 offset-lg-0">Realização</div>
-                <div class="images col-sm-6 col-md-5 offset-md-2 col-lg-6 offset-lg-0">
-                    <img class="img-thumbnail" src="{{asset('images/logoIFRN.png')}}" alt="Logo IFRN">
-                </div>
         </div>
+    </div>
 </section> --}}
 
-  
+
+
+
+
+
+
+
+<section class="py-5">
+    <div class="container my-5">
+        <div class="row mt-2">
+            <div class="col">
+                <div class="row gx-5 row-cols-1">
+                    <div class="col-sm-6 col-md-6 mb-6">
+                        <div class="h-100">
+                            <div class="card-body p-4">
+                                <p class="px-2 fw-bold text-lgbt-purple me-0">Parceiros</p>
+                                <img class="px-2 me-0" src="{{ asset('images/logo-cetra.png') }}" alt="">
+                                <img class="px-2 me-0" src="{{ asset('images/logo-centro-sabia.png') }}" alt="">
+
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-md-6 mb-6">
+                        <div class="h-100">
+                            <div class="card-body p-6">
+                                <p class="fw-bold text-lgbt-purple px-6">Realização</p>
+                                <img class="img-thumbnail px-6" src="{{ asset('images/logoIFRN.png') }}" alt="Logo IFRN">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
