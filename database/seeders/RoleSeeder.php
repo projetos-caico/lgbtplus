bac33fc4b74a54c2533ab10314395194c2acb20f
nataliaalvs <natalia.reges6@gmail.com>
2022-05-09 15:05:34 -0300

    pull

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
-use App\Models\admin;
+use App\Models\Admin;
 use App\Models\User;
 
 
