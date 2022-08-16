182a0de8f635e667a9df024b5100991b0ecb49e9
nataliaalvs <natalia.reges6@gmail.com>
2022-08-16 13:56:34 -0300

    pagina show

--- resources/views/dashboard/usuario/show.blade.php
+++ resources/views/dashboard/usuario/show.blade.php
@@ -2,15 +2,15 @@
 
 @section('main')
 <div class="d-flex h-100">
-    <div class="align-self-center mr-auto">
+    <div class="align-self-center mr-auto m-3">
         <h2>Informações sobre {{$user->name}}</h2>
     </div>
     <form action="#"> 
         <button type="submit" class="btn btn-outline-secondary mb-2">Apagar</button>
     </form>
 </div>
-<div class="col-md-8">
-    <div class="card mb-3">
+<div class="col-md-9">
+    <div class="card mb-5">
         <div class="card-body">
             <div class="row">
           <div class="col-sm-3">
