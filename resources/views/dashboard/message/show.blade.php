d11435248794c05270322de53396e81b04f8ce21
Romerito Campos de Andrade <romerito.campos@gmail.com>
2022-09-14 18:22:49 -0300

    chaging icon position

--- resources/views/dashboard/message/show.blade.php
+++ resources/views/dashboard/message/show.blade.php
@@ -8,7 +8,7 @@
 @section('main')
 
 <div class="row align-items-center">
-  <div class="col"><h2>Mensagem</h2></div>
+  <div class="col-auto mr-auto"><h2>Mensagem</h2></div>
   <div class="col-auto">
     <a href="{{route('list.email')}}">
       <i class="fas fa-mail-bulk fa-lg"></i>
@@ -16,57 +16,60 @@
   </div>
 </div>
 
-<div class="row">
-  <div class="card shadow mb-4">
-    <div class="card-header py-3">
-      <!-- Message Header -->
-      <h6 class="m-0">
-        <div class="row align-items-center">
-          <div class="col">         
-            <p style="margin: 0;">Nome: {{$message->name}}</p>
-            <p style="margin: 0;">Email: {{$message->email}}</p>
+<div class="row align-items-center">
+  <div class="col">
+    <div class="card shadow mb-4">
+      <div class="card-header py-3">
+        <!-- Message Header -->
+        <h6 class="m-0">
+          <div class="row align-items-center">
+            <div class="col">         
+              <p style="margin: 0;">Nome: {{$message->name}}</p>
+              <p style="margin: 0;">Email: {{$message->email}}</p>
+            </div>
+            <div class="col-auto">
+              <span>
+                @if ($message->status != \App\Models\Message::RESPONDIDO)
+                  <a class="btn-link text-lgbt-purple" id="reply-button">
+                    <i class="fas fa-reply fa-lg"></i>
+                  </a>    
+                @endif          
+              </span>
+            </div>
           </div>
-          <div class="col-auto">
-            <span>
-              @if ($message->status != \App\Models\Message::RESPONDIDO)
-                <a class="btn-link text-lgbt-purple" id="reply-button">
-                  <i class="fas fa-reply fa-lg"></i>
-                </a>    
-              @endif          
-            </span>
+        </h6>
+      </div>
+    
+      <div class="card-body">         
+            
+        <div class="card-text">      
+          <div class="row justify-content-end align-items-center px-3">
+            <p class="font-weight-light">{{$message->date()}}</p>
           </div>
+          <p class="text-justify">{{$message->message}}</p>
         </div>
-      </h6>
-    </div>
-  
-    <div class="card-body">         
-          
-      <div class="card-text">      
-        <div class="row justify-content-end align-items-center px-3">
-          <p class="font-weight-light">{{$message->date()}}</p>
+        <hr>
+    
+        <div>
+          <p>Situação: <b class="text-lgbt-purple">{{$message->status()}}</b></p>
         </div>
-        <p class="text-justify">{{$message->message}}</p>
+        @if ($message->status == \App\Models\Message::LIDO)     
+        <div class="card-text">
+          @include('dashboard.message.form')
+        </div> 
+        @elseif ($message->status == \App\Models\Message::RESPONDIDO)    
+        <div class="card-text">
+          <div class="row justify-content-end py-3 ">
+            <div class="col font-weight-light">{{$message->user->name}}</div>
+            <div class="col-auto">{{$message->date()}}</div>
+          </div>
+          <p class="text-justify">{{$message->awnser}}</p>
+        </div>      
+        @endif      
       </div>
-      <hr>
-  
-      <div>
-        <p>Situação: <b class="text-lgbt-purple">{{$message->status()}}</b></p>
-      </div>
-      @if ($message->status == \App\Models\Message::LIDO)     
-      <div class="card-text">
-        @include('dashboard.message.form')
-      </div> 
-      @elseif ($message->status == \App\Models\Message::RESPONDIDO)    
-      <div class="card-text">
-        <div class="row justify-content-end py-3 ">
-          <div class="col font-weight-light">{{$message->user->name}}</div>
-          <div class="col-auto">{{$message->date()}}</div>
-        </div>
-        <p class="text-justify">{{$message->awnser}}</p>
-      </div>      
-      @endif      
     </div>
   </div>
+  
 </div>
 
 
